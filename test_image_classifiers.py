3d407d1823a85c828839c7c3dc2273676e569ebc
wlyle473 <wlyle473@gmail.com>
2025-05-11 00:57:20 +0100

    feat: Added function to load model + classifier wrapper.

--- test_image_classifiers.py
+++ test_image_classifiers.py
@@ -58,23 +58,69 @@ class ModelWrapper:
         """
         images = self.image_preprocessor(images)
         features = self.feature_extractor(images)
-        outputs = self.classifier(features)
+        print("Features", [feature.shape for feature in features])
+        outputs = self.classifier(features[-1]) # Last feature map
         return features, outputs
+
+class ClassifierWrapper:
+    def __init__(
+                self, 
+                global_pool:str,
+                classifier_head:torch.nn.Module,
+                ):
+        """
+        Initialises a classifier wrapper with the specified global pooling and classifier head.
+
+        Args:
+            global_pool (str): Type of global pooling to use.
+            classifier_head (torch.nn.Module): Classifier head to use.
+        """
+        if global_pool == "avg":
+            self.global_pool = torch.nn.AdaptiveAvgPool2d(1)
+        else:
+            raise NotImplementedError(f"Global pool {global_pool} not implemented.")
+        self.classifier_head = classifier_head
+
+    def __call__(self, features:torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass features to get the classifier outputs.
+
+        Args:
+            features (torch.Tensor): Features to process.
+        """
+        pooled_features = self.global_pool(features)
+        print("Pooled features", pooled_features.shape)
+        pooled_features = pooled_features.view(pooled_features.size(0), -1)
+        print("Pooled features reshaped", pooled_features.shape)
+        outputs = self.classifier_head(pooled_features)
+        print("Outputs", outputs.shape)
+        return outputs
+        
+def load_model(model_name:str) -> ModelWrapper:
+    """
+    Loads a model from the timm library with the specified name.
+
+    Args:
+        model_name (str): Name of the model to load.
+    """
+    feature_extractor = timm.create_model(model_name=model_name, pretrained=True, in_chans=3, features_only=True)
+    classifier = timm.create_model(model_name=model_name, pretrained=True, in_chans=3, features_only=False)
+    print(dir(classifier))
+
+    classifier = ClassifierWrapper(global_pool=classifier.global_pool, classifier_head=classifier.get_classifier())
+
+    data_config = timm.data.resolve_model_data_config(feature_extractor)
+    image_preprocessor = ImagePreprocessor(data_config)
+    model = ModelWrapper(feature_extractor=feature_extractor, classifier=classifier, image_preprocessor=image_preprocessor)
+    return model
 
 if __name__ == "__main__":
     print("Testing image classifiers...")
     model_name = "maxvit_tiny_tf_224.in1k"
-    feature_extractor = timm.create_model(model_name=model_name, pretrained=True, in_chans=3, features_only=True)
-    classifier = timm.create_model(model_name=model_name, pretrained=True, in_chans=3, features_only=False).get_classifier()
-    data_config = timm.data.resolve_model_data_config(feature_extractor)
-    print("Data config:", data_config)
-
-    image_preprocessor = ImagePreprocessor(data_config)
-    model = ModelWrapper(feature_extractor=feature_extractor, classifier=classifier)
-
+    model = load_model(model_name=model_name)
     test_batch = torch.randn(4, 3, 224, 224)
 
-    preprocessed_images = image_preprocessor(test_batch)
-    print("Preprocessed images shape:", preprocessed_images.shape)
-
+    features, outputs = model.forward(test_batch)
+    print("Features", [feature.shape for feature in features])
+    print("Outputs", outputs.shape)
     print("Model created successfully.")
