cc26dbb278e9a375c7007ae7b8779ff5c6d3f298
wlyle473 <wlyle473@gmail.com>
2025-05-11 16:22:34 +0100

    fix: Issue with answer generator only working for one call.

--- test_image_classifiers.py
+++ test_image_classifiers.py
@@ -11,18 +11,6 @@ if __name__ == "__main__":
     model_name = "maxvit_tiny_tf_224.in1k"
     model = load_model(model_name=model_name)
     model.eval()
-    test_batch = torch.randn(4, 3, 224, 224)
-
-    inference_result = model.forward(test_batch)
-    features = inference_result.features
-    logits = inference_result.logits
-    probs = inference_result.probs
-    class_ids = inference_result.class_ids
-    print("Features", [feature.shape for feature in features])
-    print("Logits", logits.shape)
-    print("Probs", probs.shape)
-    print("Class IDs", class_ids.shape)
-    print("Model created successfully.")
 
     def visualise_image(image:JpegImageFile) -> None:
         """
@@ -42,7 +30,6 @@ if __name__ == "__main__":
         plt.axis('off')
         plt.show()
 
-
     def imagenet1k_sample_generator():
         """
         Generator function to yield samples from the ImageNet-1K dataset.
@@ -52,23 +39,18 @@ if __name__ == "__main__":
         imagenet_dataset = load_dataset("imagenet-1k", split="validation", streaming=True, trust_remote_code=True)
 
         for sample in imagenet_dataset:
-            print("Sample:", sample.keys())
             image = sample["image"]
             label = sample["label"]
 
             # Visualise the image
-            print(type(image), type(label))
             visualise_image(image)
 
             # Image conversion
             image = image.convert("RGB") # JpegImageFile -> PIL Image
-            print(type(image))
             image = np.array(image) # PIL Image -> np.ndarray
             image = torch.tensor(image, dtype=torch.float32) # np.ndarray -> torch.Tensor
             image = image.permute(2, 0, 1) # (H, W, C) -> (C, H, W)
             image = image.unsqueeze(0) # Add batch dimension
-            print("Image shape:", image.shape)
-            print("Label:", label)
             yield (image, label)
 
     with open("in1k_cls_index.json", "r") as f:
@@ -79,42 +61,47 @@ if __name__ == "__main__":
         Generator function to yield the answer for a sample from the ImageNet-1K dataset.
         - Returns a JSON object containing the feature, logits, confidence, predicted class name, and actual class name.
         """
-        image, label = next(imagenet1k_gen)
-        
-        # Forward pass
-        with torch.no_grad():
-            inference_result = model.forward(image)
-        features = inference_result.features
-        logits = inference_result.logits
-        probs = inference_result.probs
-        class_ids = inference_result.class_ids
 
-        assert logits.shape == (1, 1000), f"Logits shape mismatch: {logits.shape}"
-        assert probs.shape == (1, 1000), f"Probs shape mismatch: {probs.shape}"
-        assert class_ids.shape == (1,), f"Class IDs shape mismatch: {class_ids.shape}"
+        while True:
+            image, label = next(imagenet1k_gen)
+            
+            # Forward pass
+            with torch.no_grad():
+                inference_result = model.forward(image)
+            features = inference_result.features
+            logits = inference_result.logits
+            probs = inference_result.probs
+            class_ids = inference_result.class_ids
 
-        # Remove batch dimension for all tensors
-        features = [feature.squeeze(0).numpy().tolist() for feature in features] 
-        logits = logits.squeeze(0).numpy().tolist()
-        class_id = class_ids.squeeze(0).numpy().tolist()
+            assert logits.shape == (1, 1000), f"Logits shape mismatch: {logits.shape}"
+            assert probs.shape == (1, 1000), f"Probs shape mismatch: {probs.shape}"
+            assert class_ids.shape == (1,), f"Class IDs shape mismatch: {class_ids.shape}"
 
-        # Extract confidence and class names.
-        confidence = probs[0][class_id].numpy().tolist()
-        predicted_class_name = imagenet1k_cls_index[str(class_id)]
-        actual_class_name = imagenet1k_cls_index[str(label)]
+            # Remove batch dimension for all tensors
+            features = [feature.squeeze(0).numpy().tolist() for feature in features] 
+            logits = logits.squeeze(0).numpy().tolist()
+            class_id = class_ids.squeeze(0).numpy().tolist()
 
-        json_result = {
-            "feature": features,
-            "logits": logits,
-            "confidence": confidence,
-            "predicted_class_name": predicted_class_name,
-            "actual_class_name": actual_class_name,
-        }
-        print("JSON result", json_result)
-        yield json_result
+            # Extract confidence and class names.
+            confidence = probs[0][class_id].numpy().tolist()
+            predicted_class_name = imagenet1k_cls_index[str(class_id)]
+            actual_class_name = imagenet1k_cls_index[str(label)]
+
+            json_result = {
+                # "feature": features,
+                # "logits": logits,
+                "confidence": confidence,
+                "predicted_class_name": predicted_class_name,
+                "actual_class_name": actual_class_name,
+            }
+            print("JSON result", json_result)
+            yield json_result
 
     
     imagenet1k_gen = imagenet1k_sample_generator()
     answer_gen = answer_generator()
-    pred_answer = next(answer_gen)
+    
+    for i in range(5):
+        print("Iteration", i)
+        pred_answer_json = next(answer_gen)
     
