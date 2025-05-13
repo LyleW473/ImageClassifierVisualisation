f6df4f6d9cb04f01644bdeb4a16bc49a3e218cbf
wlyle473 <wlyle473@gmail.com>
2025-05-13 17:59:09 +0100

    feat: Added section component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import Heading from './components/Heading'
+import Section from './components/Section'
 
 function App() {
   return (
-    <Heading title="Hello World" />
+    <>
+      <Heading title={"Hello World"} />
+      <Section title={"Example Section"}>
+          This is an example section
+      </Section>
+    </>
   )
 }
 
